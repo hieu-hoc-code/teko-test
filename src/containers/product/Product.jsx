a68a01a7fabc3f36dca4e2c4f2fcd63c2d3f5f5e
duchieu <duchieu.ctk41@gmail.com>
2021-09-26 22:02:45 +0700

    product

--- src/containers/product/Product.jsx
+++ src/containers/product/Product.jsx
@@ -1,8 +1,10 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { fetchProducts, fetchColors } from 'redux/actions/productAction'
+import { useSelector, useDispatch } from 'react-redux'
+import convertColor from 'utils/convertColor'
+import Modal from 'components/modal/Modal'
 
 import './product.scss'
-
-import Exit from 'assets/images/cross.png'
 
 function openForm() {
   document.getElementById('popup-data').style.display = 'block'
@@ -12,34 +14,40 @@ function closeForm() {
   document.getElementById('popup-data').style.display = 'none'
   document.getElementById('tb-product').style.opacity = '1'
 }
-class Product extends Component {
-  render() {
-    return (
-      <div className="update">
-        <div className="submit">
-          <button type="submit" onClick={openForm}>
-            Submit
-          </button>
-        </div>
-        <div className="table-product" id="tb-product">
-          <table>
-            <tr className="title-pro">
-              <td>ID</td>
-              <td>Error Description</td>
-              <td>Product Image</td>
-              <td>Product Name</td>
-              <td>SKU</td>
-              <td>Color</td>
-            </tr>
-            <tr>
+
+const Product = () => {
+  const { items, colors } = useSelector((state) => state.product)
+  const dispatch = useDispatch()
+  useEffect(() => dispatch(fetchProducts()), [])
+  useEffect(() => dispatch(fetchColors()), [])
+
+  return (
+    <div className="update">
+      <div className="submit">
+        <button type="submit" onClick={openForm}>
+          Submit
+        </button>
+      </div>
+      <div className="table-product" id="tb-product">
+        <table>
+          <tr className="title-pro">
+            <td>ID</td>
+            <td>Error Description</td>
+            <td>Product Image</td>
+            <td>Product Name</td>
+            <td>SKU</td>
+            <td>Color</td>
+          </tr>
+          {items.map((item) => (
+            <tr key={item.id}>
               <td>
-                <span>01</span>
+                <span>{item.id}</span>
               </td>
               <td>
-                <span>Product eror 101</span>
+                <span>{item.errorDescription}</span>
               </td>
               <td>
-                <img src="" alt="img product"></img>
+                <img src={item.image} alt="img product"></img>
               </td>
               <td>
                 <input type="text" maxLength="30"></input>
@@ -48,43 +56,27 @@ class Product extends Component {
                 <input type="text"></input>
               </td>
               <td>
-                <select name="color" id="color">
-                  <option value="black">Black</option>
-                  <option value="White">White</option>
-                  <option value="White">Red</option>
-                  <option value="White">Green</option>
-                  <option value="White">Blue</option>
-                  <option value="White">Yellow</option>
+                <select
+                  name="color"
+                  id="color"
+                  selected={convertColor(colors, item.id)}
+                >
+                  {
+                    colors.map(color => (
+                      <option key={color.id} selected={convertColor(colors, item.id) === color.name}>{color.name}</option>
+                    ))
+                  }
+                  
                 </select>
               </td>
             </tr>
-          </table>
-        </div>
-        <div className="popup-data" id="popup-data">
-          <div className="re-uploaded">
-            <span>Re-uploaded Products</span>
-            <img src={Exit} alt="exit" onClick={closeForm}></img>
-          </div>
-          <div className="content-uploaded">
-            <div className="img-sp">
-              <img src="" alt="img-sp"></img>
-            </div>
-            <div className="information">
-              <p>Laptop Acer nitro 5</p>
-              <p>Id : 1</p>
-              <p>
-                SKU : <span>123456</span>
-              </p>
-              <p>
-                Color : <span>Black</span>
-              </p>
-            </div>
-          </div>
-          <button type="button" id="ok" onClick={closeForm}>OK</button>
-        </div>
+          ))}
+        </table>
       </div>
-    )
-  }
+      {/* modal */}
+      <Modal />
+    </div>
+  )
 }
 
 export default Product
