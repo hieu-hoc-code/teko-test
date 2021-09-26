bf961c43ad1d92b4ffb9a794ad45ff36d1bcec02
duchieu <duchieu.ctk41@gmail.com>
2021-09-26 23:26:29 +0700

    hic

--- src/containers/product/Product.jsx
+++ src/containers/product/Product.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { fetchProducts, fetchColors } from 'redux/actions/productAction'
-import { useSelector, useDispatch } from 'react-redux'
+import {
+  fetchProducts,
+  fetchColors,
+  handleItemValue,
+} from 'redux/actions/productAction'
+import { useSelector, useDispatch, connect } from 'react-redux'
 import convertColor from 'utils/convertColor'
 import Modal from 'components/modal/Modal'
 
@@ -17,23 +21,32 @@ const Product = () => {
   const submitHandle = () => {
     setIsModal(!isModal)
   }
+
+  const setItemValue = (item, key, val, index) => {
+    item[`${key}`] = val
+    dispatch(handleItemValue({item: item, index: index}))
+  }
   return (
     <div className="update">
-      <div className="submit" >
-        <button type="submit" onClick={submitHandle}>Submit</button>
+      <div className="submit">
+        <button type="submit" onClick={submitHandle}>
+          Submit
+        </button>
       </div>
       <div className="table-product" id="tb-product">
         <table>
-          <tr className="title-pro">
-            <td>ID</td>
-            <td>Error Description</td>
-            <td>Product Image</td>
-            <td>Product Name</td>
-            <td>SKU</td>
-            <td>Color</td>
-          </tr>
+          <thead>
+            <tr className="title-pro">
+              <td>ID</td>
+              <td>Error Description</td>
+              <td>Product Image</td>
+              <td>Product Name</td>
+              <td>SKU</td>
+              <td>Color</td>
+            </tr>
+          </thead>
           <tbody>
-            {items.map((item) => (
+            {items.map((item, index) => (
               <tr key={item.id}>
                 <td>
                   <span>{item.id}</span>
@@ -45,22 +58,29 @@ const Product = () => {
                   <img src={item.image} alt="img product"></img>
                 </td>
                 <td>
-                  <input type="text" maxLength="30"></input>
+                  <input
+                    type="text"
+                    maxLength="30"
+                    defaultValue={item.name}
+                  ></input>
                 </td>
                 <td>
-                  <input type="text"></input>
+                  <input
+                    type="text"
+                    defaultValue={item.sku}
+                    // onChange={(e) => setItemValue(item, 'sku', e.target.value, index) }
+                  ></input>
                 </td>
                 <td>
-                  <select
-                    name="color"
-                    id="color"
-                  >
+                  <select name="color" id="color">
                     <option value>Default</option>
                     {colors.map((color) => (
                       <option
                         key={color.id}
                         value={color.name}
-                        selected={convertColor(colors, item.color) === color.name}
+                        selected={
+                          convertColor(colors, item.color) === color.name
+                        }
                       >
                         {color.name}
                       </option>
